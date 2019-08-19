45eab2071e11c0cbfb61cf6218bab7a8e334e31a
mackdelany <mackdelany@gmail.com>
2019-08-19 13:56:30 +0200

    Refactor news.py as function

--- news.py
+++ news.py
@@ -6,29 +6,28 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-if __name__ == '__main__':
-    news = pd.read_csv(Path('data/train/', 'RedditNews_train.csv'))
+def process_text_features(filepath, max_features=200, create_csv=False, return_frame=True, csv_directory='./data/interim', csv_path='data/interim/text_features.csv'):
 
-    max_features = 200 # note this hyperparameter
-
-    # Drop NAs
-    news.dropna(inplace=True, axis=0)
-
-    # Change text to lower case
+    news = pd.read_csv(filepath).dropna(inplace=True, axis=0)
     news['News'] = news['News'].apply(lambda x: x.lower())
-
-    # Concat stories by day
     news = news.groupby(['Date'])['News'].apply(lambda x: ', '.join(x)).reset_index()
 
-    # Note max features as a hyperparameter!!
     vectorizer = TfidfVectorizer(stop_words='english', max_features=max_features)
-
     news_vectors = vectorizer.fit_transform(news['News'].value).toarray()
-
     text_features = pd.DataFrame(data=news_vectors, columns=vectorizer.get_feature_names())
 
-    # Concat vectors with dates
     text_features = pd.concat([pd.Series(news.Date.unique()).to_frame(name='Date'), text_features], axis=1)
 
-    os.makedirs('./data/interim', exist_ok=True)
-    text_features.to_csv('data/interim/text_features.csv',index=False)
+    if create_csv == True:
+        os.makedirs(csv_directory, exist_ok=True)
+        text_features.to_csv(csv_path,index=False)
+
+    if return_frame == True:
+        return text_features
+
+
+if __name__ == '__main__':
+    
+    filepath = Path('data/train/', 'RedditNews_train.csv')
+    process_text_features(filepath, create_csv=True, return_frame=False)
+
